77b8dd7153ef595ff86eeaa98605993e459f4c87
ispan <ispan@DESKTOP-PAKSETB>
2022-10-21 16:15:11 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Q1. 1+2+3+4+....+N<105 請問N是多少
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int i = 1;
+
+            while (i < 105)
+                total += i++;
+            Console.WriteLine("Sum is: " + total);
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        //Q2.使用while,在畫面顯示所有可以整除300的整數，例如:1,2,3,4,5,6,10,...,300
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 300)
+            {
+                if (300 % i == 0)
+                    Console.WriteLine(i + " ");
+                i++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
